c35813c0e17569f37cb641ca977524d97201a27a
Moong0122 <2015004802@hanyang.ac.kr>
2020-02-12 21:01:42 +0900

    styling To Do -> add emoji & interaction between emojis

--- ToDo.js
+++ ToDo.js
@@ -11,18 +11,72 @@ const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends React.Component {
   state = {
-    isEditing: false
+    isEditing: false,
+    isCompleted: false
   };
   render() {
+    const { isCompleted, isEditing } = this.state;
     return (
       <View style={styles.container}>
-        <TouchableOpacity>
-          <View style={styles.circle} />
-        </TouchableOpacity>
-        <Text style={styles.text}>Hello, I'm a TO DO</Text>
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+          <Text
+            style={[
+              styles.text,
+              isCompleted ? styles.completedText : styles.uncompletedText
+            ]}
+          >
+            Hello, I'm a TO DO
+          </Text>
+        </View>
+
+        {isEditing ? (
+          // 수정 할 때 모드
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>✅</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          // 수정 안 할 때 모드
+          <View style={styles.actions}>
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>✏️</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>❌</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
+  _toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
+      };
+    });
+  };
+  _startEditing = () => {
+    this.setState({ isEditing: true });
+  };
+  _finishEditing = () => {
+    this.setState({ isEditing: false });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -32,20 +86,47 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
     // flexDirection 덕분에 alignItem을 하면 세로 정렬이 된다
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "space-between"
   },
   circle: {
     // 원을 만들려면 borderRadius는 width, height의 절반으로 하면 된다
     width: 30,
     height: 30,
     borderRadius: 15,
-    borderColor: "red",
     borderWidth: 3,
-    marginRight: 20
+    marginRight: 20,
+    marginLeft: 10
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
   },
   text: {
     fontWeight: "600",
     fontSize: 20,
     marginVertical: 20
+  },
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "#353839"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width / 2,
+    justifyContent: "space-between"
+  },
+  actions: {
+    flexDirection: "row"
+  },
+  actionContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10
   }
 });
